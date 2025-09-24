04bef9eb5875ad88632b45324ed256f432500b1e
Genesis-Prime <redacted@primecodex.invalid>
2025-09-24 08:20:28 +0000

    Add pyrightconfig and package __init__ for Pylance improvements

---
+++ emota/__init__.py
@@ -0,0 +1,5 @@
+"""EMOTA package exposing core engine components."""
+
+from .unity import EMOTAUnityEngine  # noqa: F401
+from .braid import DesireFearBraid  # noqa: F401
+from .archetype import ArchetypalResonanceEngine  # noqa: F401
